7c95901ef0101cfa4e54a4e4abe2c1dd19bb44bb
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-04-11 09:19:56 -0700

    Create a function to display sentence as an animation

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+const typeEachCharactor = (sentence) => {
+  for (let i = 0; i < sentence.length; i++) {
+    setTimeout(() => {
+      // print the char here
+      process.stdout.write(sentence[i]);
+    }, 500 * i); // <= 1s delay to make it noticeable. Can dial it down later.
+  }
+
+  setTimeout(() => {
+    console.log();
+  }, 500 * (sentence.length + 1));
+};
+typeEachCharactor("Now it works.");
